632833b8d0e776298be1b758241b1f3987afa544
andreshf90 <andres.herrera@mail.udp.cl>
2020-05-14 10:56:16 -0400

    Ultimos Cambios

--- app/src/main/java/com/example/magicbolreal/MainActivity.java
+++ app/src/main/java/com/example/magicbolreal/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.magicbolreal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 private ImageView ballI;
 private TextView respuesta;
 
-private String[]respuestaArray={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
-        "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
-        "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
-        "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"}
+private String[]respuestaArray={"Onda Disco", "Muevete como una culebra", "Esta noche la salsa es tu pasión", "Por que no pruebas con chacha", "Baila lo que quieras", "Una romántica",
+        "La bachata hoy te domina", "Danza contemporanea", "Algo clásico", "Menea las caderas con un reggeton", "Baila porque si,lo que suene",
+        "Baila como un esqueleto", "Libera todo tu cuerpo y muevete", "Esto es el baile del robot", "Y por que no probar con la danza del canguro",
+        "Hit del momento", "Imita bailando a alguien", "Solo mueve las piernas", "muevete como si tuvieras frio"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,23 @@ private String[]respuestaArray={"Es cierto", "Definitivamente es así", "Sin dud
         ballI= findViewById(R.id.boton);
         respuesta= findViewById(R.id.respuesta);
 
-        ballI.setOnClickListener(this);
+        ballI.setOnClickListener((View.OnClickListener) this);
+        Toast.makeText(MainActivity.this, " Baila Porque Si ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void show() {
+    }
+
+
+    public void onClick(View v){
+    switch (v.getId()){
+        case R.id.boton:
+            int random= new Random().nextInt(respuestaArray.length);
+            respuesta.setText(respuestaArray[random]);
+
+            break;
+    }
 
     }
 }
