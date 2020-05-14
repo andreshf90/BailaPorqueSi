7bde33179427613c08d8de1387eb8787bb3723d0
andreshf90 <andres.herrera@mail.udp.cl>
2020-05-14 18:07:30 -0400

    Cambios finales definitivos

--- app/src/main/java/com/example/magicbolreal/MainActivity.java
+++ app/src/main/java/com/example/magicbolreal/MainActivity.java
@@ -8,16 +8,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 private ImageView ballI;
-private TextView respuesta;
+private TextView respuestabaile;
 
-private String[]respuestaArray={"Onda Disco", "Muevete como una culebra", "Esta noche la salsa es tu pasión", "Por que no pruebas con chacha", "Baila lo que quieras", "Una romántica",
+private String[] respuestaArray = {"Onda Disco", "Muevete como una culebra", "Esta noche la salsa es tu pasión", "Por que no pruebas con chacha", "Baila lo que quieras", "Una romántica",
         "La bachata hoy te domina", "Danza contemporanea", "Algo clásico", "Menea las caderas con un reggeton", "Baila porque si,lo que suene",
         "Baila como un esqueleto", "Libera todo tu cuerpo y muevete", "Esto es el baile del robot", "Y por que no probar con la danza del canguro",
         "Hit del momento", "Imita bailando a alguien", "Solo mueve las piernas", "muevete como si tuvieras frio"};
@@ -28,22 +27,19 @@ private String[]respuestaArray={"Onda Disco", "Muevete como una culebra", "Esta 
         setContentView(R.layout.activity_main);
 
         ballI= findViewById(R.id.boton);
-        respuesta= findViewById(R.id.respuesta);
+        respuestabaile= findViewById(R.id.respuesta);
 
         ballI.setOnClickListener((View.OnClickListener) this);
         Toast.makeText(MainActivity.this, " Baila Porque Si ", Toast.LENGTH_SHORT).show();
-
     }
 
-    private void show() {
-    }
-
-
+    @Override
     public void onClick(View v){
+
     switch (v.getId()){
         case R.id.boton:
             int random= new Random().nextInt(respuestaArray.length);
-            respuesta.setText(respuestaArray[random]);
+            respuestabaile.setText(respuestaArray[random]);
 
             break;
     }
